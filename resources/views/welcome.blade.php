5d54e92f375d28b51c3fe6581c48dd62ec06c27e
Meet Godhani <meet_godhani1@yahoo.com>
2016-03-21 21:50:05 -0400

    removed google analytics

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,15 +22,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.25.0/js/components/notify.min.js"></script>
 <script src="{{ elixir('js/lib.js') }}"></script>
 <script src="{{ elixir('js/app.js') }}"></script>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-57944498-3', 'auto');
-  ga('send', 'pageview');
-
-</script>
 </body>
 </html>
