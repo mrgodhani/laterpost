2cae1d8a1cb5262b25e84e8db820b2fcb2b405d4
Meet Godhani <meetgodhani23@gmail.com>
2017-07-03 22:42:11 -0400

    update on layout templats

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html class="no-js" lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>{{ config('app.name', 'Laterpost') }}</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<!--[if lte IE 11]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade
+    your browser</a> to improve your experience and security.</p>
+<![endif]-->
+<div id="app">
+    @yield('content')
+</div>
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
